c41ff789330891434efd378b2466cd3efee6dc1e
rehankhan1338 <rehanaltaf888@gmail.com>
2026-07-27 15:19:03 +0500

    database-fix

--- wp-config.php
+++ wp-config.php
@@ -21,16 +21,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress_elementor' );
+define( 'DB_NAME', 'u970484384_soulful_dbt' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'u970484384_soulful_usr' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'G0R&t7x*|j' );
 
 /** Database hostname */
-define( 'DB_HOST', '127.0.0.1' );
+define( 'DB_HOST', '194.59.164.105' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
